d9a8cfd065972ee75e31506459c430dda6768d7d
Eric Brandon Kam <ericbkam@EBKs-MacBook-Pro.local>
2019-12-17 21:01:49 -0600

    Add existing file

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,21 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, jsonify
+import json
 
-app = Flask(__name__)
+app = Flask(__name__) # create an app instance
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    name = request.args.get('name', 'World')
+    return 'Hello, {escape(name)}!'
+                           
+@app.route('/pokedex', methods=['GET'])
+def view_pokedex():
+    return jsonify(pokedex.list())         
+
+@app.route('/<name>', methods=['GET'])
+def view_pokemon(name):
+    return jsonify(pokedex[name].list())
+        
+if __name__ == 'main':
+    pokedex = request.get('pokemon.json')
+    app.run(debug=True)
